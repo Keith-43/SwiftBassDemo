89d1a74278c3f8b307fb103886d20c1da00ac6ae
Keith Bromley <kbromley@me.com>
2023-03-08 10:32:28 -0800

    Update SwiftBassDemoApp.swift

--- SwiftBassDemo/SwiftBassDemo/SwiftBassDemoApp.swift
+++ SwiftBassDemo/SwiftBassDemo/SwiftBassDemoApp.swift
@@ -2,10 +2,7 @@
 //  SwiftBassDemoApp.swift
 //  SwiftBassDemo
 //
-/// This app has crashed because it attempted to access privacy-sensitive data without a usage description.  The app's Info.plist must contain an NSMicrophoneUsageDescription key with a string value explaining to the user how the app uses this data.
-///
-//  Created by Keith Bromley on 3/7/23.
-//
+
 
 import SwiftUI
 
